25453e969fda9cbc0f16deeca8d368528970e280
2829156353@qq.com <2829156353@qq.com>
2020-09-03 18:27:34 +0800

    11111

--- server/start.py
+++ server/start.py
@@ -1,31 +1,33 @@
 from flask import Flask
-from multiprocessing import Queue
+from environs import Env
+import os,sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from server.admin.user import user
 from server.admin.home import home
-from environs import Env
-import os
-
-
-env = Env()
-env.read_env()
 
 
 
 
 
 app = Flask(__name__)
+app.register_blueprint(home,url_prefix='/')
+app.register_blueprint(user,url_prefix='/user')
 
-# app.register_blueprint(home,url_prefix='/')
-# app.register_blueprint(user,url_prefix='/user')
+# start this server from the main.py
+def start_server():
+    env_file = os.path.dirname(os.path.abspath(__file__)) + '/.env'
+    env = Env()
+    env.read_env(path=env_file)
 
+    if(not os.path.exists(env_file)):
+        raise FileNotFoundError('webserver .env file not found')
 
-# def start_server():
-#     app.run(debug=True)
+    app.run(
+        host = env.str('HOST'),
+        port = env.str('PORT'),
+    )
 
 
 
 if __name__ == "__main__":
-    print('123')
-    # evn = Env.read_env()
-    # print(evn)
-    # start_server()
+    start_server()
